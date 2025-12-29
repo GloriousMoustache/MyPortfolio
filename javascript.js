9502531394dd50a9fe510e86f98719d038582139
Richard Moustache Baker <richard.baker@skanska.co.uk>
2025-12-29 15:25:34 +0000

    Added HTML, CSS and Javascript for the responsive collapsible menu. Updated the footer with new content and styling.

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function openMenu(){
+    // let x = document.getElementById("nav-container");
+    // if (x.className === "nav-links"){
+    //     x.className += "responsive-links";
+    // }
+    // else {
+    //     x.className = "nav-links"
+    // }
+    const element = document.getElementById("nav-container");
+    element.classList.toggle("responsive-links");
+}
